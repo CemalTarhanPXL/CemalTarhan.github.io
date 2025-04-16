77062eee6343fa9e445b875d9201db43d0fabbfa
CemalTarhan038 <cemal.tarhan@student.pxl.be>
2025-04-16 17:52:16 +0200

    Configureer Next.js voor statische export op GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export', // Schakelt statische export in
-  basePath: isProd ? '/jouw-repository-naam' : '', // Geef de repository-naam op voor GitHub Pages
-  assetPrefix: isProd ? '/jouw-repository-naam/' : '', // Voor statische assets
+  basePath: isProd ? '/CemalTarhanPXL.github.io' : '', // Geef de repository-naam op voor GitHub Pages
+  assetPrefix: isProd ? '/CemalTarhanPXL.github.io' : '', // Voor statische assets
   images: {
     unoptimized: true, // Schakelt standaard afbeeldingsoptimalisatie uit
   },
